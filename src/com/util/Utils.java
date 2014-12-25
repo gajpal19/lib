e07100d456d1473eb32579703ee5db725d84ad13
Admin <Admin@TOSHIBA>
2014-12-25 14:23:42 +0530

    First commit into git repo

---
+++ src/com/util/Utils.java
@@ -0,0 +1,27 @@
+/**
+ * 
+ */
+package com.util;
+
+/**
+ * @author Admin
+ *
+ */
+public class Utils {
+
+	/**
+	 * 
+	 */
+	public Utils() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
